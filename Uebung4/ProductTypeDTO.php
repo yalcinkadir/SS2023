be9e712e320a2fc995bd3f3fd3215ebcdb22e7a4
Kadir Yalcin <ygulsun.sahin@gmail.com>
2023-06-12 13:35:53 +0200

    added new file ProductTypeDTO.php

---
+++ Uebung4/ProductTypeDTO.php
@@ -0,0 +1,12 @@
+<?php
+class ProductTypeDTO
+{
+    public $name;
+    public $url;
+
+    public function __construct($name, $url)
+    {
+        $this->name = $name;
+        $this->url = $url;
+    }
+}
