51ab230e4be8b22c36444629bfe0b016842cdb22
Kadir Yalcin <ygulsun.sahin@gmail.com>
2023-06-12 13:32:27 +0200

    added functions

--- Uebung4/ProductService.php
+++ Uebung4/ProductService.php
@@ -1,1 +1,22 @@
 <?php
+
+class ProductService
+{
+    private $model;
+
+    public function __construct($model)
+    {
+        $this->model = $model;
+    }
+
+    public function getProductTypes()
+    {
+        return $this->model->getProductTypes();
+    }
+
+    public function getProductsByTypeId($typeId)
+    {
+        return $this->model->getProductsByTypeId($typeId);
+    }
+
+}
